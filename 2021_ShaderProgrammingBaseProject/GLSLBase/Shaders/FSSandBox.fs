0b7e8a05a222447e421959b32ee3b7c33cbbac66
Jo <jkyu6554@kpu.ac.kr>
2021-04-28 07:04:14 +0900

    내부가 채워진 원 그리기

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/FSSandBox.fs
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/FSSandBox.fs
@@ -4,7 +4,19 @@ layout(location=0) out vec4 FragColor;
 
 varying vec4 v_Color;
 
+const vec3 Circle = vec3(0.5,0.5,0.0);
+
 void main()
 {
-	FragColor = v_Color;
+	vec4 color = vec4(0);
+	float tempLength = length(v_Color.rgb - Circle);
+	if(tempLength <0.5)
+	{
+		color =vec4(1,1,1,1);
+	}
+	else
+	{
+		color =vec4(0,0,0,0);
+	}
+	FragColor = color;
 }
