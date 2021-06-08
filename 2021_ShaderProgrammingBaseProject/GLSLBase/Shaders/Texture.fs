b944ae3f689e8509901b1b3d09aae5a48316c5cb
Jo <jkyu6554@kpu.ac.kr>
2021-06-09 00:27:40 +0900

    6_2 텍스처 이미지 변경 (시험)

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/Texture.fs
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/Texture.fs
@@ -6,8 +6,18 @@ uniform sampler2D u_TexSampler;
 
 in vec2 v_TexPos;
 
+const float PI = 3.141592;
+
+vec4 p1()
+{
+	vec2 newTex = v_TexPos;
+	newTex.y = abs(newTex.y-0.5)*2.0;
+	vec4 returnColor = texture(u_TexSampler,newTex);
+	return returnColor;
+}
+
 void main()
 {
-	//FragColor = vec4(1);//texture(u_TexSampler,v_TexPos)
-	FragColor = vec4(v_TexPos,0,1);
+	//FragColor = texture(u_TexSampler,v_TexPos);	
+	FragColor = p1();	
 }
