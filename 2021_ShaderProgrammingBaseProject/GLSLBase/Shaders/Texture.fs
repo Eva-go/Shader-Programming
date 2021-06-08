483211099b38f66814f9b8102f920d52737b5e78
Jo <jkyu6554@kpu.ac.kr>
2021-06-09 02:33:26 +0900

    14주차 완료

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/Texture.fs
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/Texture.fs
@@ -3,7 +3,7 @@
 layout(location=0) out vec4 FragColor;
 
 uniform sampler2D u_TexSampler;
-
+uniform float u_Step;
 in vec2 v_TexPos;
 
 const float PI = 3.141592;
@@ -35,8 +35,6 @@ vec4 p3()
 	{
 		newTex.x =(v_TexPos.x)*2+0.5;
 	}
-	
-	//newTex.y =v_TexPos.y;
 
 	else
 		newTex.x=v_TexPos.x*2;
@@ -46,8 +44,19 @@ vec4 p3()
 	return returnColor;
 }
 
+vec4 SingleTexture()
+{
+	 return texture(u_TexSampler,v_TexPos);
+}
+
+vec4 SpriteTexture()
+{
+	vec2 newTex = vec2(v_TexPos.x,u_Step/6.0 + v_TexPos.y/6.0);
+	return texture(u_TexSampler,newTex);
+}
+
 void main()
 {
 	//FragColor = texture(u_TexSampler,v_TexPos);	
-	FragColor = p3();	
+	FragColor = SpriteTexture();	
 }
